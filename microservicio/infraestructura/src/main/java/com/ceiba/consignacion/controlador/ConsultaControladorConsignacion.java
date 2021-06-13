a894ed868e857df7b81bb05861f3c462fc4b4133
hugojose14 <hugo.jose.1@hotmail.com>
2021-06-13 16:35:33 -0500

    eliminando import innecesario

--- microservicio/infraestructura/src/main/java/com/ceiba/consignacion/controlador/ConsultaControladorConsignacion.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/consignacion/controlador/ConsultaControladorConsignacion.java
@@ -3,7 +3,6 @@ package com.ceiba.consignacion.controlador;
 import com.ceiba.consignacion.consulta.ManejadorListarConsignaciones;
 import com.ceiba.consignacion.modelo.dto.DtoConsignacion;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
